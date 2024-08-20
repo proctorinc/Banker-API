bfefcea62e076f7c09c876465e5779edb3c0ba7d
Matt Proctor <proctormatt0@gmail.com>
2024-08-20 15:48:13 -0700

    Added table drop for reloading schema

--- internal/db/schema.sql
+++ internal/db/schema.sql
@@ -1,5 +1,7 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+DROP TABLE IF EXISTS users CASCADE;
+
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     username VARCHAR(255) NOT NULL UNIQUE,
