16fda6648e59a8bcf018b9b5d5ca5ac9c36eefda
Matt Proctor <proctormatt0@gmail.com>
2024-09-12 16:53:13 -0700

    Added util for masking sensitive data

---
+++ internal/graphql/utils.go
@@ -0,0 +1,24 @@
+package graphql
+
+import (
+	"bytes"
+	"strings"
+)
+
+func MaskData(data string) string {
+	length := len(data)
+
+	if length > 4 {
+		var buffer bytes.Buffer
+
+		// Write stars
+		buffer.WriteString(strings.Repeat("*", length-4))
+
+		// Write last 4 characters
+		buffer.WriteString(data[length-5 : length-1])
+
+		return buffer.String()
+	}
+
+	return data
+}
